767c77abd1e80f8201ca08d825ead87e46b23bc3
jon-verd <jonathanv0022@gmail.com>
2022-10-02 13:42:29 -0600

    Lesson 7 ; for loops

---
+++ lesson-7/for-loops.py
@@ -0,0 +1,16 @@
+# print('My name is')
+# for i in range(5) :
+#     print ('Jimmy Five Times '+ str(i))
+
+# For loop above in a while loop format
+# print('My name is')
+# i = 0
+# while i < 5:
+#     print ('Jimmy Five Times '+ str(i))
+#     i = i + 1
+
+total = 0 
+for num in range (5,-1,-1) :
+    total = total + num
+print(total)
+
