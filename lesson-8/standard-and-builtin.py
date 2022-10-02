d27c0f8cef43fbb55ce2710f97f18b95043a3f3b
jon-verd <jonathanv0022@gmail.com>
2022-10-02 14:04:08 -0600

    Lesson 9 ; a deeper dive into functions. Making my own functions

--- lesson-8/standard-and-builtin.py
+++ lesson-8/standard-and-builtin.py
@@ -1,2 +1,11 @@
-import random
-random.randint(1,10)
+import random, sys, os, math
+
+from random import *
+test = randint(1, 10)
+print(test)
+
+import pyperclip
+
+pyperclip.copy('Hello World')
+post = pyperclip.paste()
+print(post)
