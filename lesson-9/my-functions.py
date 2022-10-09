9034591d76de40e8c901037e26eebafb7675fa69
jon-verd <jonathanv0022@gmail.com>
2022-10-09 14:19:27 -0600

    lesson 9

---
+++ lesson-9/my-functions.py
@@ -0,0 +1,14 @@
+def hello():
+    print('howdy')
+    print('Howdy!!!!')
+    print('Hello there o/')
+
+hello()
+hello()
+hello()
+
+def hello(name):
+    print('hello ' + name)
+
+hello('banana')
+hello('bob')
