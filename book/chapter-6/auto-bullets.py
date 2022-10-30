d1eaaa7e86abf77360c7fc2efe41448a36a1164f
jon-verd <96271886+jon-verd@users.noreply.github.com>
2022-10-30 14:50:22 -0600

    Created a program to automatically bullet lists seperated by new lines.

---
+++ book/chapter-6/auto-bullets.py
@@ -0,0 +1,20 @@
+# This is a program that takes anything on the clipboard and automatically converts it into a bulleted list.
+
+# Example text:
+# Lists of animals
+# Lists of aquarium life
+# Lists of biologists by author abbreviation
+# Lists of cultivars
+
+# We will be using pyperclip to copy our clipboard
+import pyperclip
+myText[0] = pyperclip.paste
+# pyperclip.copy(myText)
+# after importing our text, we will split the text where a new line begins, denoted by '\n'.
+lines = myText[0].split('\n')
+for i in range(len(lines)):
+    lines[i] = '* '+ lines[1]
+pyperclip.copy(my.Text)
+# now that a star is added to the beginning of each new line, we need to rejoin the text to be able to paste it all as one value
+myText = '/n'.join(lines)
+pyperclip.copy(myText)
